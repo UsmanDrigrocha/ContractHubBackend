59f9ed265754f88beb987b94a62dfbcd5ac02380
UsmanDrigrocha <UsmanDrigrocha@Gmail.com>
2023-12-26 20:58:19 +0500

    updated sendEmail

--- server/src/utils/sendMail.js
+++ server/src/utils/sendMail.js
@@ -4,16 +4,15 @@ require('dotenv').config();
 async function mail(sendMailto, mailSubject, mailText, mailHTMLBody) {
     try {
         const transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
+            service: 'gmail', // Use a service recognized by Nodemailer (e.g., 'gmail')
             auth: {
-                user: 'cleveland.parisian@ethereal.email',
-                pass: 'cDTr4VD2vgnU77Gbcu'
+                user: process.env.MAIL_EMAIL,
+                pass: process.env.MAIL_PASSWORD
             }
         });
 
         const info = await transporter.sendMail({
-            from: '"Contract Hub 👻" <contracthub@example.com>', // sender address
+            from: `"Contract Hub 👻" ${process.env.MAIL_FROM}`, // sender address
             to: `${sendMailto}`, // recipient address
             subject: `${mailSubject} ✔`, // Subject line
             text: `${mailText}`, // plain text body
